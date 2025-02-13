6f30ebe98d6198735b8ef6ce643fe961c44fad6d
mehedimrm22 <mehedi.mrm@gmail.com>
2025-02-13 12:29:04 +0200

    request handler

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -8,13 +8,17 @@ export const createToDo = (
 ) => {};
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
+const TODOS = [];
 const createTodo = (req, res, next) => {
-    const TODOS = [];
     const text = req.body.text;
     const newTodo = new todo_1.Todo(Math.random().toString(), text);
     TODOS.push(newTodo);
     res.status(201).json({ message: "Created the todo.", createTodo: newTodo });
 };
 exports.createTodo = createTodo;
+const getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
